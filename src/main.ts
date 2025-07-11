11a1164809e83dd4ad3b71c9b221bf3aa7c4e4bf
Amit Safi <amit@Amit.amit>
2025-07-11 12:19:38 +0545

    add: api prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('market-place/api/v1');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

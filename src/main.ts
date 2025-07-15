cefc8276aaec979977bfd1b2d25d275f3159ac66
amitsafi45 <safiamit45@gmail.com>
2025-07-15 15:37:22 +0545

    add: enable cors & hide info in header

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { errorMessageExtract } from '@utils/errorMessageExtract';
 import { GlobalErrorHandlingFilter } from './utils/globalErrorHandling.filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('market-place/api/v1');
+  app.use(
+    helmet({
+      xPoweredBy: false, // Hides the X-Powered-By header or sets it to a custom value
+    }),
+  );
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       stopAtFirstError: true,
